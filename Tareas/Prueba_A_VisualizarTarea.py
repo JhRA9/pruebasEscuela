68d957deecae05a980d6e1c24ff967579c9e97a3
JhRA9 <41528203+JhRA9@users.noreply.github.com>
2025-06-11 21:46:12 -0500

    Add files via upload

---
+++ Tareas/Prueba_A_VisualizarTarea.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+try:
+    # Iniciar sesión como admin
+    driver.get("http://localhost/proyectoEscuela//login/index.php")
+    driver.find_element(By.NAME, "email").send_keys("admin@admin.com")
+    driver.find_element(By.NAME, "password").send_keys("123", Keys.ENTER)
+
+    time.sleep(5)
+
+    # Ir a la página del listado de tareas
+    driver.get("http://localhost/proyectoEscuela/admin/tareas/")
+
+    time.sleep(10)
+
+    # Verificar que la tabla de tareas se haya cargado correctamente
+    filas = driver.find_elements(By.XPATH, "//table//tbody//tr")
+
+    if len(filas) > 0:
+        print(f"Prueba exitosa: se encontraron {len(filas)} tareas en la tabla. :D")
+    else:
+        print("No se encontraron usuarios en la tabla. :'( ")
+
+except Exception as e:
+    print("Error durante la prueba :(", str(e))
+
+finally:
+    driver.quit()
