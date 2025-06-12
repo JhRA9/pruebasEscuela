d9d032ad5a2e8859b120b87dfc81a3ae8e964b0a
JhRA9 <41528203+JhRA9@users.noreply.github.com>
2025-06-11 21:47:05 -0500

    Add files via upload

---
+++ Roles/Prueba_CN_CrearRol.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
+driver = webdriver.Chrome()
+
+try:
+    # Iniciar sesión como administrador
+    driver.get("http://localhost/proyectoEscuela/login/index.php")
+    driver.find_element(By.NAME, "email").send_keys("admin@admin.com")
+    driver.find_element(By.NAME, "password").send_keys("123", Keys.ENTER)
+
+    try:        
+        nuevo_rol = "COORDINADOR"
+        
+        # Navegar a la página de creación de rol
+        driver.get("http://localhost/proyectoEscuela/admin/roles/create.php")
+
+        try:
+            input_titulo = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.NAME, "nombre_rol"))
+            )
+            input_titulo.clear()
+            input_titulo.send_keys(nuevo_rol)
+            driver.find_element(By.XPATH, "//button[@type='submit']").click()
+            time.sleep(10)
+        except TimeoutException:
+            print("❌ Error: El campo 'nombre_rol' no apareció.")
+
+        # Verificar la creación del rol fue exitosa
+        mensaje_exito = "Se registro el rol de manera correcta"
+        if mensaje_exito in driver.page_source:
+            print("✅ Prueba exitosa: rol creado correctamente.")
+        else:
+            print("❌ No se detectó mensaje de éxito.")
+    except Exception as e:
+        print("❌ Prueba fallida", str(e))
+
+except Exception as e:
+    print("❌ Error general durante la prueba:", str(e))
+
+finally:
+    driver.quit()
